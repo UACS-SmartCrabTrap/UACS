eb8367b0b19fef0d5f6084844f2ced08a1bf8776
Stephanie Kathleen Salazar <sdysart@crm.ucsc.edu>
2018-04-18 21:33:59 -0700

    Added comments to FSK Rx and FSK Alternating

--- HighFreqFSK/HighFAlternating.cydsn/main.c
+++ HighFreqFSK/HighFAlternating.cydsn/main.c
@@ -8,14 +8,9 @@
 #define FREQ(x) (CLOCK_FREQ/x)-1 //Converts frequency x to value needed by pwm
                                  //block to output at frequency x
 
-#define ENCODING_BYTE1 0xFF //Change these values if you want to specify a
-#define ENCODING_BYTE2 0xFF //different encoding or decoding. Currently set to
-#define DECODING_BYTE1 0x00 //0b1111111111111111 for the encoding and
-#define DECODING_BYTE2 0x01 //0b0000000000000001 for the decoding.
 
 #define ZERO 0x0
 #define ONE  0x1
-#define DATA_LENGTH 7
 #define DELAY_TIME  1000 //Change this value to adjust delay time between signal
                          //transmissions. Currently set to 5 seconds.
 
@@ -24,26 +19,24 @@ static int bitTime = 0;
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    /* Enable global interrupts. */
+    CyGlobalIntEnable; 
     
     /*Block initializations*/
     PWM_Modulator_Start();
     PWM_1_Start();
     PWM_2_Start();
     PWM_Switch_Timer_Start();
-    
-    /*Variable initializations*/
-    int bitCase = 0;
-    int data_turn = 0;
-    unsigned int data_to_be_sent = ONE;
+   
     
     for(;;)
     {
+        /*Alternate between high and low frequency for 1 ms*/
         PWM_Modulator_WritePeriod(FREQ(ONE_FREQ));
-        PWM_Modulator_WriteCompare((FREQ(ONE_FREQ))/2); // Sets pulse width
+        PWM_Modulator_WriteCompare((FREQ(ONE_FREQ))/2); // Sets pulse width to half
         CyDelay(1);
         PWM_Modulator_WritePeriod(FREQ(ZERO_FREQ));
-        PWM_Modulator_WriteCompare((FREQ(ZERO_FREQ))/2); // Sets pulse width
+        PWM_Modulator_WriteCompare((FREQ(ZERO_FREQ))/2); // Sets pulse width to half
         CyDelay(1);
         
         
@@ -51,16 +44,4 @@ int main(void)
     
 }//end main()
 
-
-
-
-
-/*
- * function: int Byte(unsigned int hex_value, int bT)
- * parameters: hex_value - a byte specifying what data you want to send
- *             bT - the current bit time
- * returns: bitCase - a high or low signal to be sent to an output pin
- * description: This function takes in a hex value and sends it out a bit at a
- *  time as a high or low signal depending on the bit time.
- */
 
--- HighFreqFSK/HighFSKRx.cydsn/main.c
+++ HighFreqFSK/HighFSKRx.cydsn/main.c
@@ -1,40 +1,44 @@
 /* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * Smart Crab Trap
+ * FSK RX 
+ * Edited by: Stephanie Salazar
+ * Revision: 4/18/18
  *
  * ========================================
 */
+
 #include "project.h"
 #include <stdio.h>
 #include "LCD_Char.h"
 
+// Interrupt for switching bits 500 ms
 CY_ISR_PROTO(HighF_LevelCount);
-static uint16 levelCounter = 0;
-static uint8 zeroCount = 0; 
-static uint8 oneCount = 0; 
-static uint8 currentBit = 0;
-static uint8 dataCount = 0;
-static uint8 data = 0;
-static uint8 crabs = 0;
-static uint8 dataFlag = 0;
-static uint8 decodeFlag = 0;
-static uint8 decodeWrong = 0;
 
-//FLAGS
-static uint8 lcdFlagEncode = 0;
-static uint8 lcdFlagData = 0;
-static uint8 lcdFlagDecode = 0;
+// Global Variables
+static uint16 levelCounter = 0; // Timer counter to debounce bit
+static uint8 zeroCount = 0; // 0 count used in timer counter debounce
+static uint8 oneCount = 0; // 1 count used in timer counter debounce
+static uint8 currentBit = 0; // x/10 bit decision for 500 ms bit
+static uint8 dataCount = 0; // which bit of data we are looking at
+static uint8 data = 0; // 4 bits of data
+static uint8 crabs = 0; // 4 bits of data transferred from data variable
+static uint8 dataFlag = 0; // Flag to start looking for data
+static uint8 decodeFlag = 0; // Flag to start looking for post-fix
+
+
+//FLAGS for turning on messages on LCD screen
+static uint8 lcdFlagEncode = 0; // Turns on pre-fix message
+static uint8 lcdFlagData = 0; // Displays data 
+static uint8 lcdFlagDecode = 0; // good or bad postfix
+static uint8 decodeWrong = 0;
 
 
 int main(void)
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+    /* Enable global interrupts. */
+    CyGlobalIntEnable; 
 
+    /* initialization/startup code here */
     PWM_Recon_Start();
     HighF_BPF_Comp_Start();
     HighF_ShiftReg2_Start();
@@ -44,21 +48,22 @@ int main(void)
     HighF_LevelCountISR_StartEx(HighF_LevelCount);
     LCD_Char_Start();
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    // Displays Loading Message before receiving pre-fix
     char OutputString[12];
     char display[12];
     sprintf(display, "counting crabs...");
-    LCD_Char_Position(0u,0u);
+    LCD_Char_Position(0u,0u); // Resets cursor to top of LCD Screen
     LCD_Char_PrintString(display);
 
     for(;;)
     {
-        
-        /* Place your application code here. */
+        // LCD Screen Messages
+        // If encode is received, display message
         if(lcdFlagEncode == 1){
             LCD_Char_ClearDisplay();
             LCD_Char_PrintString("0xFF pre-fix");
-            lcdFlagEncode = 0;
+            lcdFlagEncode = 0; 
+        // When 4 bits are received, data will display at top of screen
         }else if(lcdFlagData == 1){
             sprintf(OutputString, "%i", crabs);
             LCD_Char_ClearDisplay();
@@ -67,6 +72,7 @@ int main(void)
             LCD_Char_PrintString(" crabs");
             dataFlag = 0;
             lcdFlagData = 0;
+        // Postfix will display good or bad below data on screen
         }else if(lcdFlagDecode == 1){
             LCD_Char_Position(1u,0u);
             char8 displayG[] = "good";
@@ -79,40 +85,41 @@ int main(void)
             LCD_Char_PrintString(displayB);
             decodeWrong = 0;
         }
-    }
-}
+    } // end of for(;;)
+} // end of main()
     
 
-//Bit length = 0.001 s
-//timer period = 0.0001 s
+//Bit length = 500 ms
+//timer period = 50 ms
 //will check bit 10 times, to debounce
 CY_ISR(HighF_LevelCount){
     levelCounter++;
     
+    // Count whether bit is currently 1 or 0
     if(HighF_OutComp_GetCompare() != 0){
         oneCount++;
     }else{
         zeroCount++;
     }
     
-    //debouncing 
+    // Debouncing
     if(levelCounter == 10){
-        if(oneCount >= 7){
+        if(oneCount >= 7){ // 1 bit must be 7/10 = 1 
             currentBit = 0x01;
             oneCount = 0;
         
-        }else{
+        }else{ // if oneCount <= 7, bit is 0
             currentBit = 0x00; 
             zeroCount = 0; 
         }
-        dataCount++;
-        levelCounter = 0;
-        //CountOut_Write(currentBit);
+        dataCount++; // Keep track of what bit we are on
+        levelCounter = 0; // Reset timer bit debouncer  
         
         //Look for pre-fix
-        data = data << 1;
+        data = data << 1; // Shift data over to store next bit
         data = data | currentBit;
-        if((data == 0xff) && (dataFlag == 0) && (decodeFlag == 0)){
+        // Check if data is prefix(oxFF) but we are not looking for data or decode
+        if((data == 0xff) && (dataFlag == 0) && (decodeFlag == 0)){ 
             HighF_CountOut_Write(1);
             dataCount = 0;
             data = 0x00;
@@ -120,7 +127,7 @@ CY_ISR(HighF_LevelCount){
             lcdFlagEncode = 1; //Display pre-fix on lcd
 
         
-        // Look for data
+        // Look for 4 bits of data
         }else if((dataFlag == 1) && (dataCount > 3)){
             crabs = data;
             data = 0; //Restart data for decode
@@ -130,16 +137,18 @@ CY_ISR(HighF_LevelCount){
             decodeFlag = 1; //Now go to decode stage
         }
         
+        // Check for 8 bits of post-fix
         if(decodeFlag == 1 && (dataCount > 7)){
+            // Correct postfix is 0x01
             if(data == 0x01){
-             lcdFlagDecode = 1;
+             lcdFlagDecode = 1; // lcd flag for "good" post-fix
             }else{
-                decodeWrong = 1;
+                decodeWrong = 1; // lcd flag for "bad" post-fix
             }
             decodeFlag = 0;
             dataFlag = 0; //Don't want to check for data anymore
         }
-    }
-}
+    } // end of if(levelCounter == 10)
+} // end of CY_ISR(HighF_LevelCount)
 
 /* [] END OF FILE */

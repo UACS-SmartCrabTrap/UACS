108eb70e591cc445bd701f30ebfd068953af6a40
Julian Concepcion Salazar <jucsalaz@crm.ucsc.edu>
2018-05-07 16:54:38 -0700

    Added matlab function to determine receiver battery life

---
+++ Julian_MATLAB/RxBatteryLife.m
@@ -0,0 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% File: RxBatteryLife.m
+% Author: Julian Salazar
+% Date: 05/07/18
+% Purpose: To date, the battery life of the receiver module is unknown due
+%   to a few variables, most important of which is how long it will be on
+%   throughout a day. This file will serve as a function to determine
+%   battery life depending on these variables.
+% Edits:
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function BatteryLife = RxBatteryLife(OnTime) %OnTime in seconds (0-86400)
+
+c = 675; %Battery capacity in mAh
+v = 5; %Voltage supplied from the battery
+i = 22; %Current draw from the receiver module in mA
+i_s = .12; %Sleep current drawn from circuit when not receiving data
+% V = 3.3 %Voltage supplied from the battery
+% I = 17.5 %Current draw from the receiver module in mA
+s = 86400; %Seconds in a day
+
+%Equation gives average current drawn from the circuit over a day
+i_day = ( i * (OnTime/s) ) + ( i_s * ((s-OnTime)/s) );
+
+%Gives battery life in days
+BatteryLife = (c / i_day) / 24;

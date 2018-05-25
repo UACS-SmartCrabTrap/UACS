b34d945ffd9796cda308f99b8604cb865a4afaf1
Ivonne Patricia Fajardo <ipfajard@crm.ucsc.edu>
2018-05-24 17:44:37 -0700

    reverted to previous commit. accidental checkout

---
+++ HighFreqFSK/HighFAlternating.cydsn/main.c
@@ -0,0 +1,109 @@
+/* ========================================
+ * Smart Crab Trap
+ * FSK RX 
+ * Edited by: Stephanie Salazar
+ * Revision: 4/23/18
+ * Function: This code alternates between two frequencies depending on 
+ * the set delay time. This can be changed with the #define DELAY_TIME. 
+ * There are 3 PWM blocks with this functionality that have the same ONE_FREQ,
+ * but vary depending on the ZERO_FREQ. The audible PWM is for testing.
+ * ========================================
+*/
+
+/*Included header files*/
+#include "project.h"
+
+/*Definitions*/
+#define CLOCK_FREQ 1000000 //Adjust this to match PWM_Clock in top design
+#define ONE_FREQ 45000 //Frequency sent to give logic 1 (in Hz)
+#define ZERO_FREQ_1 30000 //Frequency sent to give logic 0 (in Hz)
+#define ZERO_FREQ_2 30000 //Frequency sent to give logic 0 (in Hz)
+#define ZERO_FREQ_3 38000 //Frequency sent to give logic 0 (in Hz)
+#define FREQ(x) (CLOCK_FREQ/x)-1 //Converts frequency x to value needed by pwm
+                                 //block to output at frequency x
+
+
+#define ZERO 0x0
+#define ONE  0x1
+#define DELAY_TIME  10 //Change this value to adjust delay time between signal
+                         //transmissions. Currently set to 10ms seconds.
+
+CY_ISR_PROTO(isr_msec); // High F  Alternating Interrupts
+
+// Set alternating frequency
+static int alternating = 1;
+
+int main(void)
+{
+    /* Enable global interrupts. */
+    CyGlobalIntEnable; 
+    
+    /*Block initializations*/
+    PWM_Alternating_1_Start();
+    PWM_Alternating_2_Start();
+    PWM_Alternating_3_Start();
+    Audible_Frequency_Start();
+    PWM_Alternating_Timer_Start();
+    
+    isr_msec_StartEx(isr_msec);
+    
+    // Pin 1.6 PWM
+    PWM_Alternating_3_WritePeriod(FREQ(ONE_FREQ));
+    PWM_Alternating_3_WriteCompare((FREQ(ONE_FREQ))/2); // Sets pulse width to half
+    
+    for(;;)
+    {
+        if(alternating == 1){
+            /*Alternate between high and low frequency for 10 ms*/
+            // Pin 1.2 PWM
+            PWM_Alternating_1_Start();
+            PWM_Alternating_1_WritePeriod(FREQ(ONE_FREQ));
+            PWM_Alternating_1_WriteCompare((FREQ(ONE_FREQ))/2); // Sets pulse width to half
+            // Pin 1.4 PWM
+            PWM_Alternating_2_Start();
+            PWM_Alternating_2_WritePeriod(FREQ(ONE_FREQ));
+            PWM_Alternating_2_WriteCompare((FREQ(ONE_FREQ))/2); // Sets pulse width to half
+            // Pin 1.6 PWM
+            PWM_Alternating_3_Start();
+            PWM_Alternating_3_WritePeriod(FREQ(ONE_FREQ));
+            PWM_Alternating_3_WriteCompare((FREQ(ONE_FREQ))/2); // Sets pulse width to half
+            
+
+        }else if(alternating == 2 || alternating == 4){
+            PWM_Alternating_2_Stop();
+        }else if(alternating == 3){
+            // Pin 1.2 PWM
+            PWM_Alternating_1_Start();
+            PWM_Alternating_1_WritePeriod(FREQ(ZERO_FREQ_1));
+            PWM_Alternating_1_WriteCompare((FREQ(ZERO_FREQ_1))/2); // Sets pulse width to half
+            // Pin 1.4 PWM
+            PWM_Alternating_2_Start();
+            PWM_Alternating_2_WritePeriod(FREQ(ZERO_FREQ_2));
+            PWM_Alternating_2_WriteCompare((FREQ(ZERO_FREQ_2))/2); // Sets pulse width to half
+            // Pin 1.6 PWM
+            PWM_Alternating_3_Start();
+            PWM_Alternating_3_WritePeriod(FREQ(ZERO_FREQ_3));
+            PWM_Alternating_3_WriteCompare((FREQ(ZERO_FREQ_3))/2); // Sets pulse width to half
+        }
+            
+    }//end for(;;)
+    
+}//end main()
+
+// Interrupt triggered on a 10 ms timer timeout
+// Will change between 30k and 45k every 10 ms
+CY_ISR(isr_msec)
+{
+    if(alternating == 1){
+        alternating++;
+    }else if(alternating == 2){
+        alternating++;
+    }else if(alternating == 3){
+        alternating++;
+    }else if(alternating == 4){
+        alternating = 1;
+    }
+
+}//end CY_ISR(isr_msec)
+
+
---
+++ HighFreqFSK/HighFSKRx.cydsn/main.c
@@ -0,0 +1,156 @@
+/* ========================================
+ * Smart Crab Trap
+ * FSK RX 
+ * Edited by: Stephanie Salazar
+ * Revision: 5/10/18
+ * Function: This project takes in a signal from an outside source
+ * and reads the data within the signal. This code waits for a pre-fix
+ * of 0xFF and then reads the next 4 bits as the data and confirms
+ * the message with a post-fix of 0x01. The data is display on an LCD display.
+ * ========================================
+*/
+
+#include "project.h"
+#include <stdio.h>
+#include "LCD_Char.h"
+
+// Interrupt for switching bits 100 ms
+CY_ISR_PROTO(Bit_Timer);
+
+// Global Variables
+static uint16 levelCounter = 0; // Timer counter to debounce bit
+static uint8 zeroCount = 0; // 0 count used in timer counter debounce
+static uint8 oneCount = 0; // 1 count used in timer counter debounce
+static uint8 currentBit = 0; // x/10 bit decision for 500 ms bit
+static uint8 dataCount = 0; // which bit of data we are looking at
+static uint8 data = 0; // 4 bits of data
+static uint8 crabs = 0; // 4 bits of data transferred from data variable
+static uint8 dataFlag = 0; // Flag to start looking for data
+static uint8 decodeFlag = 0; // Flag to start looking for post-fix
+
+
+// FLAGS for turning on messages on LCD screen
+static uint8 lcdFlagEncode = 0; // Turns on pre-fix message
+static uint8 lcdFlagData = 0; // Displays data 
+static uint8 lcdFlagDecode = 0; // good or bad postfix
+static uint8 decodeWrong = 0;
+
+
+int main(void)
+{
+    /* Enable global interrupts. */
+    CyGlobalIntEnable; 
+
+    /* initialization/startup code here */
+    PWM_Recon_Start();
+    BPF_Comp_Start();
+    Shift_Reg_Start();
+    Out_Comp_Start();
+    Bit_Timer_Start();
+    Timer_ISR_StartEx(Bit_Timer);
+    LCD_Char_Start();
+
+    // Displays Loading Message before receiving pre-fix
+    char OutputString[12];
+    char display[12];
+    sprintf(display, "counting crabs...");
+    LCD_Char_Position(0u,0u); // Resets cursor to top of LCD Screen
+    LCD_Char_PrintString(display);
+
+    for(;;)
+    {
+        // LCD Screen Messages
+        // If encode is received, display message
+        if(lcdFlagEncode == 1){
+            LCD_Char_ClearDisplay();
+            LCD_Char_PrintString("0xFF pre-fix");
+            lcdFlagEncode = 0; 
+        // When 4 bits are received, data will display at top of screen
+        }else if(lcdFlagData == 1){
+            sprintf(OutputString, "%i", crabs);
+            LCD_Char_ClearDisplay();
+            LCD_Char_Position(0u,0u);
+            LCD_Char_PrintString(OutputString);
+            LCD_Char_PrintString(" crabs");
+            dataFlag = 0;
+            lcdFlagData = 0;
+        // Postfix will display good or bad below data on screen
+        }else if(lcdFlagDecode == 1){
+            LCD_Char_Position(1u,0u);
+            char8 displayG[] = "good";
+            LCD_Char_PrintString(displayG);
+            dataFlag = 0;
+            lcdFlagDecode = 0;
+        }else if(decodeWrong == 1){
+            LCD_Char_Position(1u,0u);
+            char8 displayB[] = "bad";
+            LCD_Char_PrintString(displayB);
+            decodeWrong = 0;
+        }
+    } // end of for(;;)
+} // end of main()
+    
+
+//Bit length = 100 ms
+//timer period = 10 ms
+//will check bit 10 times, to debounce
+CY_ISR(Bit_Timer){
+    levelCounter++;
+    
+    // Count whether bit is currently 1 or 0
+    if(Out_Comp_GetCompare() != 0){
+        oneCount++;
+    }else{
+        zeroCount++;
+    }
+    
+    // Debouncing
+    if(levelCounter == 10){
+        if(oneCount >= 7){ // 1 bit must be 7/10 = 1 
+            currentBit = 0x01;
+            oneCount = 0;
+        
+        }else{ // if oneCount <= 7, bit is 0
+            currentBit = 0x00; 
+            zeroCount = 0; 
+        }
+        dataCount++; // Keep track of what bit we are on
+        levelCounter = 0; // Reset timer bit debouncer  
+        
+        //Look for pre-fix
+        data = data << 1; // Shift data over to store next bit
+        data = data | currentBit;
+        // Check if data is prefix(oxFF) but we are not looking for data or decode
+        if((data == 0xff) && (dataFlag == 0) && (decodeFlag == 0)){ 
+            Count_Out_Write(1);
+            dataCount = 0;
+            data = 0x00;
+            dataFlag = 1; //Start looking for data
+            lcdFlagEncode = 1; //Display pre-fix on lcd
+
+        
+        // Look for 4 bits of data
+        }else if((dataFlag == 1) && (dataCount > 3)){
+            crabs = data;
+            data = 0; //Restart data for decode
+            dataCount = 0; 
+            lcdFlagEncode = 0; //Turn off encode message
+            lcdFlagData = 1; //Display data
+            decodeFlag = 1; //Now go to decode stage
+        }
+        
+        // Check for 8 bits of post-fix
+        if(decodeFlag == 1 && (dataCount > 7)){
+            // Correct postfix is 0x01
+            if(data == 0x01){
+             lcdFlagDecode = 1; // lcd flag for "good" post-fix
+            }else{
+                decodeWrong = 1; // lcd flag for "bad" post-fix
+            }
+            decodeFlag = 0;
+            dataFlag = 0; //Don't want to check for data anymore
+        }
+    } // end of if(levelCounter == 10)
+} // end of CY_ISR(HighF_LevelCount)
+
+/* [] END OF FILE */

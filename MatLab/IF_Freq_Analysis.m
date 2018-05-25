4cc16a296080d69100c20d41ce79bcc80a2dda21
Ivonne Patricia Fajardo <ipfajard@crm.ucsc.edu>
2018-05-24 17:14:03 -0700

    Freq analysis and bit time graphs

--- MatLab/IF_Freq_Analysis.m
+++ MatLab/IF_Freq_Analysis.m
@@ -7,57 +7,57 @@
 
 %Read in .csv file from Agilent Discovery 2
 %Ignore first 11 lines: parameter information not data
-data = csvread('42_surface_raw_gain.csv', 11, 0); 
+data = csvread('SSpsocsharedpower_gain_lpfout.csv', 11, 0); 
 
 %Determine size of incoming data file 
 %r = number of rows, c = number of columns 
 [r,c] = size(data);
 
-%{  Data column 1: time measurements 
-    Data column 2: Channel 1 data
-    Data column 3: Channel 2 data 
-%}
+%     Data column 1: time measurements 
+%     Data column 2: Channel 1 data
+%     Data column 3: Channel 2 data 
+
 time = data(:,1);
 channel1 = data(:,2);
 channel2 = data(:,3);
 
 %============== Calculations ==================================================================================
 
-%{  IF_Zero_Crossings: Find zero crossings after DC bias removed
-    In:   Signal data in Volts, time data in s
-    Out:  Voltages interpreted as '0' , times of zero crossings, channel voltages 0V biased 
-    function [ ZerosOut,TimeOut, unBaisedV ] = IF_Zero_Crossings( waveIn, TimeIn )
-%}
+%     IF_Zero_Crossings: Find zero crossings after DC bias removed
+%     In:   Signal data in Volts, time data in s
+%     Out:  Voltages interpreted as '0' , times of zero crossings, channel voltages 0V biased 
+%     function [ ZerosOut,TimeOut, unBaisedV ] = IF_Zero_Crossings( waveIn, TimeIn )
+
 
 [chan1_zeros, chan1_zeroTime, chan1_notBiased] = IF_Zero_Crossings(channel1, time);
 [chan2_zeros, chan2_zeroTime, chan2_notBiased] = IF_Zero_Crossings(channel2, time);
 
-%{  IF_FrequencyCalcs: Use zero crossing data to find frequencies 
-    In:   Time (s) of signal zero crossings from IF_Zero_Crossings(waveIn, TimeIn)
-    Out:  The frequencies in the signal, and approx. time of frequency change 
-    function [ FrequencyOut, timeChange ] = IF_FrequencyCalcs( TimeIn )
-%}
+%     IF_FrequencyCalcs: Use zero crossing data to find frequencies 
+%     In:   Time (s) of signal zero crossings from IF_Zero_Crossings(waveIn, TimeIn)
+%     Out:  The frequencies in the signal, and approx. time of frequency change 
+%     function [ FrequencyOut, timeChange ] = IF_FrequencyCalcs( TimeIn )
+
 
 [chan1_frequencies, chan1_freqTime] = IF_FrequencyCalcs(chan1_zeroTime); 
 [chan2_frequencies, chan2_freqTime] = IF_FrequencyCalcs(chan2_zeroTime);
 
 
-%{  adjacentAverage: Take average of adjacent frequencies 
-                      if very close together in magnitude or time  
-    In:   Frequencies of signal and time changes from IF_FrequencyCalcs( TimeIn )
-    Out:  Average of frequencies in the signal, and approx. time of frequency change 
-    function [ avgFreq, avgTime ] = adjacentAverage( inFreq, inTime )
-%}
+%     adjacentAverage: Take average of adjacent frequencies 
+%                       if very close together in magnitude or time  
+%     In:   Frequencies of signal and time changes from IF_FrequencyCalcs( TimeIn )
+%     Out:  Average of frequencies in the signal, and approx. time of frequency change 
+%     function [ avgFreq, avgTime ] = adjacentAverage( inFreq, inTime )
+
 
 [chan1_freqAvg , chan1_timeAvg] = adjacentAverage(chan1_frequencies,  chan1_freqTime);
 [chan2_freqAvg , chan2_timeAvg] = adjacentAverage(chan2_frequencies,  chan2_freqTime);
 
 
-%{  freqChange: Filter out frequency changes inside a hysterisis band 
-    In:   Average frequencies of signal and time changes from adjacentAverage( inFreq, inTime )
-    Out:  Filtered average frequencies and time of frequency change   
-    function [ changeFreq , changeTime ] = freqChange( freqsIn, timesIn )
-%}
+%     freqChange: Filter out frequency changes inside a hysterisis band 
+%     In:   Average frequencies of signal and time changes from adjacentAverage( inFreq, inTime )
+%     Out:  Filtered average frequencies and time of frequency change   
+%     function [ changeFreq , changeTime ] = freqChange( freqsIn, timesIn )
+
 
 [ chan1_filterAvg, chan1_timeChange ] = freqChange( chan1_freqAvg , chan1_timeAvg );
 [ chan2_filterAvg, chan2_timeChange ] = freqChange(chan2_freqAvg , chan2_timeAvg );
